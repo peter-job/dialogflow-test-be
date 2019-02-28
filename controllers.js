86365f9f11dd8480836d978b972598fefe164208
Sebastian Martin <sebastianmartin@Sebastians-MacBook-Pro.local>
2019-02-28 20:32:05 +0000

    create postLocation funciton

--- controllers.js
+++ controllers.js
@@ -1,4 +1,9 @@
 exports.getLocation = (req, res, next) => {
-  console.log("we got a request");
-  res.status(200).send({ msg: "England" });
+	console.log('we got a request');
+	res.status(200).send({msg: 'England'});
+};
+
+exports.postLocation = (req, res, next) => {
+	console.log('we got a post requst');
+	res.status(201).send({msg: 'we added your loco loco locationnnn'});
 };
